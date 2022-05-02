943aa7e87ce065c96ff12369337d4a7d59924af6
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-02 19:01:33 +0530

    initial commits

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
-# from . import db
+from . import db
 
-# class BIRTHS(db.Model):
-#     pass
+class BIRTHS(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(300))
+    extract = db.Column(db.String(8000))
+    link = db.Column(db.String(1000))
+    date = db.Column(db.String(15))
 
 # class DEATHS(db.Model):
 #     pass
