2471ca0be73db7eb8b83e59e49c823c1a2919bb5
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-05 18:07:55 +0530

    check_db

--- app/views.py
+++ app/views.py
@@ -1,18 +1,43 @@
+from datetime import datetime as dt
+from sqlalchemy import extract
 from . import db
 from .models import *
-from flask import Blueprint, render_template, request
+from .helpers import utils, api_endpoints
+from flask import Blueprint, redirect, render_template, request
 
 views = Blueprint("views", __name__)
 
 @views.route('/', methods = ['GET'])
 def home():
     if request.method == "GET":
-        add = BIRTHS(title = "Princess Charlotte of Cambridge", extract = "Princess Charlotte of Cambridge is a member of the British royal family. She is the second child and only daughter of Prince William, Duke of Cambridge, and Catherine, Duchess of Cambridge. She is fourth in the line of succession to the British throne.", link = "https://en.wikipedia.org/wiki/Princess_Charlotte_of_Cambridge", date = "05-02-2015")
-        db.session.add(add)
-        db.session.commit()
-        record = BIRTHS.query.first()
-        print(record.title)
-        print(record.extract)
-        print(record.link)
-        print(record.date)
+        return redirect("/births")
+
+@views.route('/births', methods = ['GET'])
+def births():
+    if request.method == "GET":
+        records = WikiData.query.all()
+        current_date = dt.now().strftime("%d-%m")
+        if len(records) == 0 or records.first().date[:5] != current_date:
+            response = utils.get_api_data(utils.generate_endpoint(api_endpoints.ENDPOINTS.births))["births"]
+            data = []
+            for item in response:
+                category = "births"
+                page = item["pages"][0]
+                extract = page["extract"]
+                title = page["normalizedtitle"]
+                link = page["content_urls"]["desktop"]["page"]
+                date = current_date + "-" + str(item["year"])
+                data.append(
+                    WikiData(category = category, title = title, extract = extract, link = link, date = date)
+                    )
+            
+            db.session.add_all(data)
+            db.session.commit()
+        
+        records = WikiData.query.all()
+        for item in records:
+            print(item.date)
+            print(item.title)
+            print(item.link)
+        
         return render_template("index.html")
