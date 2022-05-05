666febf5d54be4a1bedad986831b631daf6f5a89
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-05 18:08:18 +0530

    new_db_model

--- app/models.py
+++ app/models.py
@@ -1,20 +1,9 @@
 from . import db
 
-class BIRTHS(db.Model):
+class WikiData(db.Model):
     id = db.Column(db.Integer, primary_key = True)
+    category = db.Column(db.String(30))
     title = db.Column(db.String(300))
     extract = db.Column(db.String(8000))
     link = db.Column(db.String(1000))
     date = db.Column(db.String(15))
-
-# class DEATHS(db.Model):
-#     pass
-
-# class EVENTS(db.Model):
-#     pass
-
-# class HOLIDAYS(db.Model):
-#     pass
-
-# class SELECTED(db.Model):
-#     pass
