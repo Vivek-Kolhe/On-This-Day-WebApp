19e0153f0033d959167338ab3e99f22a7d44f924
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-05 18:08:45 +0530

    removed_useless_funcs

--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -2,11 +2,8 @@ import requests
 from datetime import datetime as dt
 from .api_endpoints import ENDPOINTS
 
-def check_date(current_date):
-    today = dt.now().strftime("%d-%m-%Y")
-    if today != current_date:
-        return False
-    return True
+def generate_endpoint(api_endpoint):
+    return api_endpoint + dt.now().strftime("/%m/%d")
 
 def get_api_data(api_endpoint):
     response = requests.get(api_endpoint, headers = {"content-type": "application/json"})
@@ -14,5 +11,14 @@ def get_api_data(api_endpoint):
         return response.json()
     return None
 
-def fetch_new_date():
-    pass
+def check_empty_db(db_model):
+    records = db_model.query.all()
+    if len(records) == 0:
+        return True
+    return False
+
+def check_date(current_date):
+    today = dt.now().strftime("%d-%m")
+    if today != current_date:
+        return False
+    return True
