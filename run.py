09b4f0efb376821c5c8e7b05942cc633c266aea1
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-07 19:16:16 +0530

    app_context

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from app import create_app, db
+from app.models import WikiData
+from app.helpers import utils
 
 app = create_app()
 
 if __name__ == "__main__":
+    with app.app_context():
+        data = utils.fetch_data(WikiData)
+        db.session.add_all(data)
+        db.session.commit()
     app.run(debug = True)
