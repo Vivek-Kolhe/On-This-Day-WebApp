74e3159971a14ea12e75354cc8cf073aad84cdc3
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-10 18:10:48 +0530

    utils.py

--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -11,25 +11,22 @@ def get_api_data(api_endpoint):
         return response.json()
     return None
 
-def fetch_data(db_model):
-    current_date = dt.now().strftime("%d-%m")
-    records = db_model.query.all()
+def fetch_data(db_model, current_date):
     data = []
-    if len(records) == 0 or records[0].date[:5] != current_date:
-        response = get_api_data(generate_endpoint(ENDPOINTS.births))["births"]
-        for item in response:
-            category = "BIRTH"
-            page = item["pages"][0]
-            extract = page["extract"]
-            title = page["normalizedtitle"]
-            link = page["content_urls"]["desktop"]["page"]
-            date = current_date
-            try:
-                year = item["year"]
-                date = date + "-" + str(year)
-            except:
-                year = ""
-            data.append(
-                db_model(category = category, title = title, extract = extract, link = link, date = date)
-                )
+    response = get_api_data(generate_endpoint(ENDPOINTS.births))["births"]
+    for item in response:
+        category = "BIRTH"
+        page = item["pages"][0]
+        extract = page["extract"]
+        title = page["normalizedtitle"]
+        link = page["content_urls"]["desktop"]["page"]
+        date = current_date
+        try:
+            year = item["year"]
+            date = date + "-" + str(year)
+        except:
+            date = date + "-XXXX"
+        data.append(
+            db_model(category = category, title = title, extract = extract, link = link, date = date)
+            )
     return data
