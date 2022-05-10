063fd3b241d3969d72f9d938dc74baf4f3cf682c
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-10 19:14:47 +0530

    no_need_for_this_file_but_meh

--- app/helpers/api_endpoints.py
+++ app/helpers/api_endpoints.py
@@ -1,6 +1,2 @@
 class ENDPOINTS:
-    births = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births"
-    deaths = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/deaths"
-    events = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/events"
-    holidays = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/holidays"
-    selected = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/selected"
+    all = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all"
