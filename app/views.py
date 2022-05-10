b5dc78ed106653bd1348089ab6c34975ae2b4c72
Vivek Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2022-05-10 18:04:36 +0530

    updated_views

--- app/views.py
+++ app/views.py
@@ -12,15 +12,22 @@ def home():
         return redirect("/births")
 
 @views.route('/births', methods = ['GET'])
-@views.route('/births/<int:page>', methods = ['GET'])
+@views.route('/births/<int:page>', methods = ['GET'], strict_slashes = False)
 def births(page = 1):
     page = page
     per_page = 10
     if request.method == "GET":
-        data = utils.fetch_data(WikiData)
-        db.session.add_all(data)
-        db.session.commit()
-        
+        records = WikiData.query.all()
+        current_date = dt.now().strftime("%d-%m")
+        if len(records) == 0 or records[0].date[:5] != current_date:
+            if records[0].date[:5] != current_date:
+                for record in records:
+                    db.session.delete(record)
+                db.session.commit()
+            data = utils.fetch_data(WikiData, current_date)
+            db.session.add_all(data)
+            db.session.commit()
+            
         records = WikiData.query.paginate(page, per_page, error_out = False)
         # for item in records:
         #     print(item.date)
